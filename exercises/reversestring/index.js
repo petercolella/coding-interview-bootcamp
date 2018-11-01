19627e559b7e0dd5fe614f769a314614de23ec27
petercolella <peter.joseph.colella@gmail.com>
2018-10-31 23:30:41 -0400

    Lecture 8.1

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,7 +7,18 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    
+  const reversedStr = [];
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr.push(str[i]);
+  }
+  console.log(reversedStr);
+  const result = reversedStr.join('');
+  console.log(result);
+  return result;
 }
+
+const word = 'apple';
+
+reverse(word);
 
 module.exports = reverse;
