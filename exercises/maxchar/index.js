008316d3417244f118e50edbb2d20d0f3eba4dfc
petercolella <peter.joseph.colella@gmail.com>
2018-11-05 23:51:53 -0500

    Lecture 18

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  charCount = 0;
+  charIndexCount = 0;
+  for (let char of str) {
+    charCountMax = char;
+    // charIndexCount = 0;
+    for (let charIndex of str) {
+      if (charIndex === char) {
+        charIndexCount++;
+        charCount = charIndexCount;
+      }
+      if (charIndexCount > charCount) {
+        // charCount = charIndexCount;
+        charCountMax = charIndex;
+      }
+      console.log(char, charIndex, charIndexCount, charCountMax);
+    }
+  }
+  console.log('finalCharCountMax: ', charCountMax);
+  return charCountMax;
+}
+
+maxChar('abcccccccd');
 
 module.exports = maxChar;
