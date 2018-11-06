61e25c69d4c73e90a66e4d1ea676c4b18d67063d
petercolella <peter.joseph.colella@gmail.com>
2018-11-06 00:58:37 -0500

    maxChar solution with loop within loop

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,50 +5,47 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-// function maxChar(str) {
-//   charCount = 0;
-//   charIndexCount = 0;
-//   for (let char of str) {
-//     charCountMax = char;
-//     // charIndexCount = 0;
-//     for (let charIndex of str) {
-//       if (charIndex === char) {
-//         charIndexCount++;
-//         charCount = charIndexCount;
-//       }
-//       if (charIndexCount > charCount) {
-//         // charCount = charIndexCount;
-//         charCountMax = charIndex;
-//       }
-//       console.log(char, charIndex, charIndexCount, charCountMax);
-//     }
-//   }
-//   console.log('finalCharCountMax: ', charCountMax);
-//   return charCountMax;
-// }
-
 function maxChar(str) {
-  const chars = {};
   let max = 0;
   let maxChar = '';
   for (let char of str) {
-    // if (!chars[char]) {
-    //   chars[char] = 1;
-    // } else {
-    //   chars[char]++;
-    // }
-    chars[char] = chars[char] + 1 || 1;
-  }
-
-  for (let charsProp in chars) {
-    if (chars[charsProp] > max) {
-      max = chars[charsProp];
-      maxChar = charsProp;
+    charIndexMax = 0;
+    for (let charIndex of str) {
+      if (charIndex === char) {
+        charIndexMax++;
+      }
+      if (charIndexMax > max) {
+        max = charIndexMax;
+        maxChar = charIndex;
+      }
     }
   }
 
   return maxChar;
 }
+
+// function maxChar(str) {
+//   const chars = {};
+//   let max = 0;
+//   let maxChar = '';
+//   for (let char of str) {
+//     // if (!chars[char]) {
+//     //   chars[char] = 1;
+//     // } else {
+//     //   chars[char]++;
+//     // }
+//     chars[char] = chars[char] + 1 || 1;
+//   }
+
+//   for (let charsProp in chars) {
+//     if (chars[charsProp] > max) {
+//       max = chars[charsProp];
+//       maxChar = charsProp;
+//     }
+//   }
+
+//   return maxChar;
+// }
 
 maxChar('abcccccccd');
 
