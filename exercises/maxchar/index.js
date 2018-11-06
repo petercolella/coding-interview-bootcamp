4b9f7b28bc30abb6e931b2fc8eee28f42241682f
petercolella <peter.joseph.colella@gmail.com>
2018-11-06 00:22:18 -0500

    Lecture 21

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -29,13 +29,25 @@
 
 function maxChar(str) {
   const chars = {};
+  let max = 0;
+  let maxChar = '';
   for (let char of str) {
-    if (!chars[char]) {
-      chars[char] = 1;
-    } else {
-      chars[char]++;
+    // if (!chars[char]) {
+    //   chars[char] = 1;
+    // } else {
+    //   chars[char]++;
+    // }
+    chars[char] = chars[char] + 1 || 1;
+  }
+
+  for (let charsProp in chars) {
+    if (chars[charsProp] > max) {
+      max = chars[charsProp];
+      maxChar = charsProp;
     }
   }
+
+  return maxChar;
 }
 
 maxChar('abcccccccd');
