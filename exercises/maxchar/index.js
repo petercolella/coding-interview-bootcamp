e1ba847b67683650cb613baf81c7a2cbb18ecf2a
petercolella <peter.joseph.colella@gmail.com>
2018-11-05 23:58:25 -0500

    Lecture 19

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,26 +5,37 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+// function maxChar(str) {
+//   charCount = 0;
+//   charIndexCount = 0;
+//   for (let char of str) {
+//     charCountMax = char;
+//     // charIndexCount = 0;
+//     for (let charIndex of str) {
+//       if (charIndex === char) {
+//         charIndexCount++;
+//         charCount = charIndexCount;
+//       }
+//       if (charIndexCount > charCount) {
+//         // charCount = charIndexCount;
+//         charCountMax = charIndex;
+//       }
+//       console.log(char, charIndex, charIndexCount, charCountMax);
+//     }
+//   }
+//   console.log('finalCharCountMax: ', charCountMax);
+//   return charCountMax;
+// }
+
 function maxChar(str) {
-  charCount = 0;
-  charIndexCount = 0;
+  const chars = {};
   for (let char of str) {
-    charCountMax = char;
-    // charIndexCount = 0;
-    for (let charIndex of str) {
-      if (charIndex === char) {
-        charIndexCount++;
-        charCount = charIndexCount;
-      }
-      if (charIndexCount > charCount) {
-        // charCount = charIndexCount;
-        charCountMax = charIndex;
-      }
-      console.log(char, charIndex, charIndexCount, charCountMax);
+    if (!chars[char]) {
+      chars[char] = 1;
+    } else {
+      chars[char]++;
     }
   }
-  console.log('finalCharCountMax: ', charCountMax);
-  return charCountMax;
 }
 
 maxChar('abcccccccd');
