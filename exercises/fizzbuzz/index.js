433d77ef4bebdc1c00cc123bfabc8cc9bb7c0474
petercolella <peter.joseph.colella@gmail.com>
2018-11-08 23:21:41 -0500

    Lecture 23 his solution

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,15 +12,29 @@
 //   4
 //   buzz
 
+// function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++) {
+//     i % 15 === 0
+//       ? console.log('fizzbuzz')
+//       : i % 5 === 0
+//         ? console.log('buzz')
+//         : i % 3 === 0
+//           ? console.log('fizz')
+//           : console.log(i);
+//   }
+// }
+
 function fizzBuzz(n) {
   for (let i = 1; i <= n; i++) {
-    i % 15 === 0
-      ? console.log('fizzbuzz')
-      : i % 5 === 0
-        ? console.log('buzz')
-        : i % 3 === 0
-          ? console.log('fizz')
-          : console.log(i);
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i);
+    }
   }
 }
 
