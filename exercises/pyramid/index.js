3a8ed80f8b4df89d30691537f45d8d8e9f9b845f
petercolella <peter.joseph.colella@gmail.com>
2018-11-21 18:01:01 -0500

    Lecture 42 first solution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -69,22 +69,40 @@
 //   }
 // }
 
-function pyramid(n) {
-  const midpoint = Math.floor((2 * n - 1) / 2);
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
 
-  for (let row = 0; row < n; row++) {
-    let level = '';
+//   for (let row = 0; row < n; row++) {
+//     let level = '';
 
-    for (let column = 0; column < 2 * n - 1; column++) {
-      if (midpoint - row <= column && midpoint + row >= column) {
-        level += '#';
-      } else {
-        level += ' ';
-      }
-    }
+//     for (let column = 0; column < 2 * n - 1; column++) {
+//       if (midpoint - row <= column && midpoint + row >= column) {
+//         level += '#';
+//       } else {
+//         level += ' ';
+//       }
+//     }
 
-    console.log(level);
+//     console.log(level);
+//   }
+// }
+
+function pyramid(n, row = 0, level = '') {
+  if (n === row) {
+    return;
   }
+
+  if (2 * n - 1 === level.length) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  const add =
+    midpoint - row <= level.length && midpoint + row >= level.length
+      ? '#'
+      : ' ';
+  pyramid(n, row, level + add);
 }
 
 module.exports = pyramid;
