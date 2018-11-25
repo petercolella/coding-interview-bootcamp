654834c91bd932ad8981dbfce50ae5f707489b99
petercolella <peter.joseph.colella@gmail.com>
2018-11-24 23:08:49 -0500

    Lecture 45

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -25,17 +25,22 @@
 //   return vowelsArr.length;
 // }
 
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u'];
+
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
 function vowels(str) {
-  let count = 0;
-  const checker = ['a', 'e', 'i', 'o', 'u'];
-
-  for (let char of str.toLowerCase()) {
-    if (checker.includes(char)) {
-      count++;
-    }
-  }
-
-  return count;
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
