76d675c39fd4011434ef4fb54265ab0fa2bc8b95
petercolella <peter.joseph.colella@gmail.com>
2018-11-29 01:57:54 -0500

    Lecture 56

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -42,6 +42,20 @@
 //   }
 // }
 
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
 function fib(n) {
   if (n < 2) {
     return n;
@@ -49,5 +63,7 @@ function fib(n) {
 
   return fib(n - 1) + fib(n - 2);
 }
+
+fib = memoize(fib);
 
 module.exports = fib;
