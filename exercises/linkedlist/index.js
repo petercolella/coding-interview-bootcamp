5bc8ffff0cd261cfc66f07a330c5c76faf5e054a
petercolella <peter.joseph.colella@gmail.com>
2018-12-03 23:50:07 -0500

    Lecture 73

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -17,6 +17,23 @@ class LinkedList {
   insertFirst(data) {
     this.head = new Node(data, this.head);
   }
+
+  size() {
+    const list = this;
+    let count = 0;
+    for (let node in list) {
+      console.log(node);
+      console.log(list);
+      console.log(list[node]);
+      console.log(count);
+      if (list[node]) {
+        count++;
+        list[node].size();
+      } else {
+        return count;
+      }
+    }
+  }
 }
 
 module.exports = { Node, LinkedList };
