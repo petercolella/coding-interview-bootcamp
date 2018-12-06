62393d5815854b93a80aa0941ab495a397798bb0
petercolella <peter.joseph.colella@gmail.com>
2018-12-05 23:46:35 -0500

    Lecture 77

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -50,6 +50,16 @@ class LinkedList {
   getFirst() {
     return this.head;
   }
+
+  getLast() {
+    let node = this.head;
+
+    while (node.next) {
+      node = node.next;
+    }
+
+    return node;
+  }
 }
 
 module.exports = { Node, LinkedList };
