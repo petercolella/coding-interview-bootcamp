b70b531127013358fec1d991f2a2897e494006d6
petercolella <peter.joseph.colella@gmail.com>
2018-12-06 00:10:52 -0500

    Lecture 81

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -68,6 +68,14 @@ class LinkedList {
   clear() {
     this.head = null;
   }
+
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+
+    this.head = this.head.next;
+  }
 }
 
 module.exports = { Node, LinkedList };
