30454c72ee261d45e50ed50d3b7def023f07c92e
petercolella <peter.joseph.colella@gmail.com>
2018-12-10 23:17:56 -0500

    Lecture 93

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -15,7 +15,8 @@ class LinkedList {
   }
 
   insertFirst(data) {
-    this.head = new Node(data, this.head);
+    // this.head = new Node(data, this.head);
+    this.insertAt(data, 0);
   }
 
   //   size() {
@@ -48,21 +49,23 @@ class LinkedList {
   }
 
   getFirst() {
-    return this.head;
+    // return this.head;
+    return this.getAt(0);
   }
 
   getLast() {
-    if (!this.head) {
-      return null;
-    }
+    // if (!this.head) {
+    //   return null;
+    // }
 
-    let node = this.head;
-    while (node) {
-      if (!node.next) {
-        return node;
-      }
-      node = node.next;
-    }
+    // let node = this.head;
+    // while (node) {
+    //   if (!node.next) {
+    //     return node;
+    //   }
+    //   node = node.next;
+    // }
+    return this.getAt(this.size() - 1);
   }
 
   clear() {
