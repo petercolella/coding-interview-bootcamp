3cd14337eb83d0c582625c04f8136ef3b565abaa
petercolella <peter.joseph.colella@gmail.com>
2018-12-12 23:58:20 -0500

    Lecture 101

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -19,10 +19,12 @@ function circular(list) {
   while (fast.next && fast.next.next) {
     slow = slow.next;
     fast = fast.next.next;
+
     if (slow === fast) {
       return true;
     }
   }
+
   return false;
 }
 
