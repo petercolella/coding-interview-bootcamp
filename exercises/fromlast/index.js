edce4bc9843e8600529cfa8d95c3fdf87013a029
petercolella <peter.joseph.colella@gmail.com>
2018-12-13 22:50:42 -0500

    Lecture 103

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,9 +11,26 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
+// function fromLast(list, n) {
+//   let slow = list.getAt(0);
+//   let fast = list.getAt(n);
+
+//   while (fast.next) {
+//     slow = slow.next;
+//     fast = fast.next;
+//   }
+
+//   return slow;
+// }
+
 function fromLast(list, n) {
-  let slow = list.getAt(0);
-  let fast = list.getAt(n);
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while (n > 0) {
+    fast = fast.next;
+    n--;
+  }
 
   while (fast.next) {
     slow = slow.next;
