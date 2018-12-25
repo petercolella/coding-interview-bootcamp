3ee8073886d7111352281c24bb5dd0ad53cbf60f
petercolella <peter.joseph.colella@gmail.com>
2018-12-24 22:41:16 -0500

    Lecture 124

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -3,28 +3,59 @@
 // Events class.  The Events class should
 // have methods 'on', 'trigger', and 'off'.
 
+// class Events {
+//   constructor() {
+//     this.events = {};
+//   }
+//   // Register an event handler
+//   on(eventName, callback) {
+//     this.events[eventName] = [];
+//     this.events[eventName].push(callback);
+//   }
+
+//   // Trigger all callbacks associated
+//   // with a given eventName
+//   trigger(eventName) {
+//     for (let i in this.events[eventName]) {
+//       this.events[eventName][i];
+//     }
+//   }
+
+//   // Remove all event handlers associated
+//   // with the given eventName
+//   off(eventName) {
+//     this.events[eventName] = [];
+//   }
+// }
+
 class Events {
   constructor() {
     this.events = {};
   }
   // Register an event handler
   on(eventName, callback) {
-    this.events[eventName] = [];
-    this.events[eventName].push(callback);
+    if (this.events[eventName]) {
+      this.events[eventName].push(callback);
+    } else {
+      this.events[eventName] = [callback];
+    }
   }
 
   // Trigger all callbacks associated
   // with a given eventName
   trigger(eventName) {
-    for (let i in this.events[eventName]) {
-      this.events[eventName][i];
+    if (this.events[eventName]) {
+      for (let cb of this.events[eventName]) {
+        cb();
+      }
     }
   }
 
   // Remove all event handlers associated
   // with the given eventName
   off(eventName) {
-    this.events[eventName] = [];
+    // this.events[eventName] = [];
+    delete this.events[eventName];
   }
 }
 
