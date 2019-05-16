3312cecb56fc110527dec3aa5d947dace7714bcc
petercolella <peter.joseph.colella@gmail.com>
2019-05-16 14:08:40 -0400

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -16,33 +16,46 @@
 //   return result;
 // }
 
+function chunk(array, size) {
+  const chunked = [];
+
+  for (let element of array) {
+    // declare a variable for the last array in the new chunked array
+    const last = chunked[chunked.length - 1];
+    // if chunked is an empty array (last doesn't exist) or the length of last has reached the appropriate size
+    if (!last || last.length === size) {
+      // a new array of the element is pushed into the chunked array
+      chunked.push([element]);
+    } else {
+      //or the element will be pushed into the last array
+      last.push(element);
+    }
+  }
+  return chunked;
+}
+
 // function chunk(array, size) {
 //   const chunked = [];
+//   let index = 0;
 
-//   for (let element of array) {
-//     const last = chunked[chunked.length - 1];
-
-//     if (!last || last.length === size) {
-//       chunked.push([element]);
-//     } else {
-//       last.push(element);
-//     }
+//   while (index < array.length) {
+//     chunked.push(array.slice(index, index + size));
+//     index += size;
 //   }
 
 //   return chunked;
 // }
 
-function chunk(array, size) {
-  const chunked = [];
-  let index = 0;
-
-  while (index < array.length) {
-    chunked.push(array.slice(index, index + size));
-    index += size;
-  }
-
-  return chunked;
-}
+// function chunk(arr, length) {
+//   const newArr = [];
+//   for (i = 0; i < arr.length; i += 0) {
+//     if (arr.length > length) {
+//       let subArr = arr.splice(i, length);
+//       newArr.push(subArr);
+//     } else newArr.push(arr.splice(0));
+//   }
+//   return newArr;
+// }
 
 // const test = [1, 2, 3, 4, 5, 6, 7, 8];
 
